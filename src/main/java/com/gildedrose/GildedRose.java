7b5164087520fd354c15b95736773649ba74ecbe
Alex <owner@slaand.com>
2025-04-17 12:54:48 +0300

    refactor if checks

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,32 +16,27 @@ class GildedRose {
             // Default
 
             if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-
                 item.sellIn = item.sellIn - 1;
+                item.quality = item.quality + 1;
 
                 if (item.sellIn < 0) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    item.quality = item.quality + 1;
+                }
+
+                if (item.quality > 50) {
+                    item.quality = 50;
                 }
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
+                if (item.sellIn < 6) {
+                    item.quality = item.quality + 3;
+                } else if (item.sellIn < 11) {
+                    item.quality = item.quality + 2;
+                } else {
                     item.quality = item.quality + 1;
+                }
 
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+                if (item.quality > 50) {
+                    item.quality = 50;
                 }
 
                 item.sellIn = item.sellIn - 1;
@@ -52,16 +47,14 @@ class GildedRose {
             } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 // legendary item, no quality and sellIn changes
             } else {
-                if (item.quality > 0) {
+                item.sellIn = item.sellIn - 1;
+                item.quality = item.quality - 1;
+                if (item.sellIn < 0) {
                     item.quality = item.quality - 1;
                 }
 
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                if (item.quality < 0) {
+                    item.quality = 0;
                 }
             }
         }
